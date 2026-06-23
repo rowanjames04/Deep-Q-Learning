fe55a0b9d15b88f1245244994e5b950ff7fb3f1b
rowanjames04 <r04james@outlook.com>
2026-06-23 21:48:38 +1000

    feat: Implement Epsilon-Greedy Algorithm

--- agent.py
+++ agent.py
@@ -5,6 +5,7 @@ from dqn import DQN
 from experiment_replay import ReplayMemory
 import itertools
 import yaml
+import random
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu' #this can probably be deleted later
 
@@ -29,27 +30,41 @@ class Agent:
         num_actions = env.action_space.n
 
         rewards_per_episode = []
+        epsilon_history = []
         
-        policy_dqn = DQN(num_states, num_actions).to_device(device)
+        policy_dqn = DQN(num_states, num_actions).to(device)
 
         if is_training:
             memory = ReplayMemory(self.replay_memory_size)
+
+            epsilon = self.epsilon_init
         
 
         for episode in itertools.count():
             state, _ = env.reset()
+            state = torch.tensor(state, dtype=torch.float, device=device)
+
             terminated = False
             episode_reward = 0.0
 
             while not terminated:
-                # Next action:
-                # (feed the observation to your agent here)
-                action = env.action_space.sample() #sample function pulls random action == 0 -> do nothing, 1 -> flap
+                
+                if is_training and random.random() < epsilon:
+                    action = env.action_space.sample() #sample function pulls random action == 0 -> do nothing, 1 -> flap
+                    action = torch.tensor(action, dtype=torch.int64, device=device)
+                else:
+                    with torch.no_grad():
+                        action = policy_dqn(state.unsqueeze(dim=0)).squeeze().argmax()
 
                 # Processing:
-                new_state, reward, terminated, _, info = env.step(action) #executes action
+                new_state, reward, terminated, _, info = env.step(action.item()) #executes action
                 
+                #Accumulate reward
                 episode_reward += reward
+
+                #Convert new state and reward to tensors on device
+                new_state = torch.tensor(new_state, dtype=torch.float, device=device)
+                reward = torch.tensor(reward, dtype=torch.float, device=device)
 
                 if is_training:
                     memory.append((state, action, new_state, reward, terminated))
@@ -57,3 +72,11 @@ class Agent:
                 state = new_state
 
             rewards_per_episode.append(episode_reward)
+
+            epsilon = max(epsilon * self.epsilon_decay, self.epsilon_min)
+            epsilon_history.append(epsilon)
+
+
+if __name__ == '__main__':
+    agent = Agent("cartpole1")
+    agent.run(is_training=True, render=False)
